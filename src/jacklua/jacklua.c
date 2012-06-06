4018018a16354228705c67ff7fce8958133a0d07
Daniel Thompson <daniel@redfelineninja.org.uk>
2012-06-06 21:06:36 +0100

    jacklua: Add some jack boilerplate and fix the build system to get it linked
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/jacklua/jacklua.c
+++ src/jacklua/jacklua.c
@@ -11,8 +11,156 @@
  * (at your option) any later version.
  */
 
-int main()
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <signal.h>
+#include <unistd.h>
+#include <jack/jack.h>
+
+jack_port_t *input_port, *output_port;
+jack_client_t *client;
+
+
+static int process (jack_nframes_t nframes, void *arg)
 {
-	return 0;
+	jack_default_audio_sample_t *in, *out;
+	int i;
+
+	in = (jack_default_audio_sample_t*)jack_port_get_buffer (input_port, nframes);
+	out = (jack_default_audio_sample_t*)jack_port_get_buffer (output_port, nframes);
+
+	for( i=0; i<nframes; i++ ) {
+		jack_default_audio_sample_t sample;
+		int quantized_sample;
+
+		sample = in[i] * (1<<14);
+		quantized_sample = (int) sample;
+		sample = (jack_default_audio_sample_t) quantized_sample;
+		out[i] = sample * (1.0f / (1<<14));
+	}
+
+	return 0;      
 }
 
+/**
+ * JACK calls this shutdown_callback if the server ever shuts down or
+ * decides to disconnect the client.
+ */
+static void jack_shutdown (void *arg)
+{
+	exit (1);
+}
+
+static void signal_handler(int sig)
+{
+	jack_client_close(client);
+	fprintf(stderr, "signal received, exiting ...\n");
+	exit(0);
+}
+
+int main (int argc, char *argv[])
+{
+	const char *client_name;
+	const char *server_name = NULL;
+	jack_options_t options = JackNullOption;
+	jack_status_t status;
+
+	if (argc >= 2) {		/* client name specified? */
+		client_name = argv[1];
+		if (argc >= 3) {	/* server name specified? */
+			server_name = argv[2];
+            int my_option = JackNullOption | JackServerName;
+			options = (jack_options_t)my_option;
+		}
+	} else {			/* use basename of argv[0] */
+		client_name = strrchr(argv[0], '/');
+		if (client_name == 0) {
+			client_name = argv[0];
+		} else {
+			client_name++;
+		}
+	}
+
+	/* open a client connection to the JACK server */
+
+	client = jack_client_open (client_name, options, &status, server_name);
+	if (client == NULL) {
+		fprintf (stderr, "jack_client_open() failed, "
+			 "status = 0x%2.0x\n", status);
+		if (status & JackServerFailed) {
+			fprintf (stderr, "Unable to connect to JACK server\n");
+		}
+		exit (1);
+	}
+	if (status & JackServerStarted) {
+		fprintf (stderr, "JACK server started\n");
+	}
+	if (status & JackNameNotUnique) {
+		client_name = jack_get_client_name(client);
+		fprintf (stderr, "unique name `%s' assigned\n", client_name);
+	}
+
+	/* tell the JACK server to call `process()' whenever
+	   there is work to be done.
+	*/
+
+	jack_set_process_callback (client, process, NULL);
+
+	/* tell the JACK server to call `jack_shutdown()' if
+	   it ever shuts down, either entirely, or if it
+	   just decides to stop calling us.
+	*/
+
+	jack_on_shutdown (client, jack_shutdown, 0);
+
+	/* create two ports */
+
+	input_port = jack_port_register (client, "input",
+					  JACK_DEFAULT_AUDIO_TYPE,
+					  JackPortIsInput, 0);
+
+	output_port = jack_port_register (client, "output",
+					  JACK_DEFAULT_AUDIO_TYPE,
+					  JackPortIsOutput, 0);
+
+	if ((input_port == NULL) || (output_port == NULL)) {
+		fprintf(stderr, "no more JACK ports available\n");
+		exit (1);
+	}
+
+	/* Tell the JACK server that we are ready to roll.  Our
+	 * process() callback will start running now. */
+
+	if (jack_activate (client)) {
+		fprintf (stderr, "cannot activate client");
+		exit (1);
+	}
+
+        /* install a signal handler to properly quits jack client */
+#ifdef WIN32
+	signal(SIGINT, signal_handler);
+	signal(SIGABRT, signal_handler);
+	signal(SIGTERM, signal_handler);
+#else
+	signal(SIGQUIT, signal_handler);
+	signal(SIGTERM, signal_handler);
+	signal(SIGHUP, signal_handler);
+	signal(SIGINT, signal_handler);
+#endif
+
+	/* keep running until the Ctrl+C */
+
+	while (1) {
+	#ifdef WIN32 
+		Sleep(1000);
+	#else
+		sleep (1);
+	#endif
+	}
+
+	jack_client_close (client);
+	exit (0);
+}
