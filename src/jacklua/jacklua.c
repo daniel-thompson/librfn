ec283aaa888ec23e1ade8e4ec6cb3ffad6d8c8ba
Daniel Thompson <daniel@redfelineninja.org.uk>
2012-06-07 07:45:00 +0100

    jacklua: Connect the tintamp top-level processing to jack
    At this point we can test the amplifier in a real-time environment
    (although currently this is limited solely to cabinet simulation).
    
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/jacklua/jacklua.c
+++ src/jacklua/jacklua.c
@@ -18,29 +18,36 @@
 #include <math.h>
 #include <signal.h>
 #include <unistd.h>
+
 #include <jack/jack.h>
 
-jack_port_t *input_port, *output_port;
-jack_client_t *client;
+#include <libtt.h>
 
+typedef struct {
+	jack_client_t *client;
+
+	jack_port_t *input_port;
+	jack_port_t *output_port;
+
+	tt_context_t tt_ctx;
+	tt_tintamp_t tintamp;
+
+	tt_sbuf_t *input_buf;
+	tt_sbuf_t *output_buf;
+} appctx_t;
+
+appctx_t appctx;
 
 static int process (jack_nframes_t nframes, void *arg)
 {
-	jack_default_audio_sample_t *in, *out;
-	int i;
+	appctx_t *ctx = arg;
 
-	in = (jack_default_audio_sample_t*)jack_port_get_buffer (input_port, nframes);
-	out = (jack_default_audio_sample_t*)jack_port_get_buffer (output_port, nframes);
+	jack_default_audio_sample_t *in = jack_port_get_buffer (ctx->input_port, nframes);
+	jack_default_audio_sample_t *out = jack_port_get_buffer (ctx->output_port, nframes);
 
-	for( i=0; i<nframes; i++ ) {
-		jack_default_audio_sample_t sample;
-		int quantized_sample;
-
-		sample = in[i] * (1<<14);
-		quantized_sample = (int) sample;
-		sample = (jack_default_audio_sample_t) quantized_sample;
-		out[i] = sample * (1.0f / (1<<14));
-	}
+	tt_float_to_sbuf(in, nframes, ctx->input_buf);
+	tt_tintamp_process(&ctx->tintamp, ctx->input_buf, ctx->output_buf);
+	tt_sbuf_to_float(ctx->output_buf, out, nframes);
 
 	return 0;      
 }
@@ -56,13 +63,17 @@ static void jack_shutdown (void *arg)
 
 static void signal_handler(int sig)
 {
-	jack_client_close(client);
+	appctx_t *ctx = &appctx;
+
+	jack_client_close(ctx->client);
 	fprintf(stderr, "signal received, exiting ...\n");
 	exit(0);
 }
 
 int main (int argc, char *argv[])
 {
+	appctx_t *ctx = &appctx;
+
 	const char *client_name;
 	const char *server_name = NULL;
 	jack_options_t options = JackNullOption;
@@ -86,8 +97,8 @@ int main (int argc, char *argv[])
 
 	/* open a client connection to the JACK server */
 
-	client = jack_client_open (client_name, options, &status, server_name);
-	if (client == NULL) {
+	ctx->client = jack_client_open (client_name, options, &status, server_name);
+	if (ctx->client == NULL) {
 		fprintf (stderr, "jack_client_open() failed, "
 			 "status = 0x%2.0x\n", status);
 		if (status & JackServerFailed) {
@@ -99,7 +110,7 @@ int main (int argc, char *argv[])
 		fprintf (stderr, "JACK server started\n");
 	}
 	if (status & JackNameNotUnique) {
-		client_name = jack_get_client_name(client);
+		client_name = jack_get_client_name(ctx->client);
 		fprintf (stderr, "unique name `%s' assigned\n", client_name);
 	}
 
@@ -107,60 +118,57 @@ int main (int argc, char *argv[])
 	   there is work to be done.
 	*/
 
-	jack_set_process_callback (client, process, NULL);
+	jack_set_process_callback (ctx->client, process, ctx);
 
 	/* tell the JACK server to call `jack_shutdown()' if
 	   it ever shuts down, either entirely, or if it
 	   just decides to stop calling us.
 	*/
 
-	jack_on_shutdown (client, jack_shutdown, 0);
+	jack_on_shutdown (ctx->client, jack_shutdown, 0);
 
 	/* create two ports */
 
-	input_port = jack_port_register (client, "input",
-					  JACK_DEFAULT_AUDIO_TYPE,
-					  JackPortIsInput, 0);
+	ctx->input_port = jack_port_register (ctx->client, "input",
+			JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
 
-	output_port = jack_port_register (client, "output",
-					  JACK_DEFAULT_AUDIO_TYPE,
-					  JackPortIsOutput, 0);
+	ctx->output_port = jack_port_register (ctx->client, "output",
+			JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
 
-	if ((input_port == NULL) || (output_port == NULL)) {
+	if ((ctx->input_port == NULL) || (ctx->output_port == NULL)) {
 		fprintf(stderr, "no more JACK ports available\n");
 		exit (1);
 	}
 
+	/* setup tintamp */
+	tt_context_init(&ctx->tt_ctx);
+	ctx->tt_ctx.sampling_frequency = jack_get_sample_rate(ctx->client);
+	ctx->tt_ctx.grain_size = jack_get_buffer_size(ctx->client);
+
+	tt_tintamp_init(&ctx->tintamp, &ctx->tt_ctx);
+
+	ctx->input_buf = tt_sbuf_new(ctx->tt_ctx.grain_size);
+	ctx->output_buf = tt_sbuf_new(ctx->tt_ctx.grain_size);
+
 	/* Tell the JACK server that we are ready to roll.  Our
 	 * process() callback will start running now. */
 
-	if (jack_activate (client)) {
+	if (jack_activate (ctx->client)) {
 		fprintf (stderr, "cannot activate client");
 		exit (1);
 	}
 
         /* install a signal handler to properly quits jack client */
-#ifdef WIN32
-	signal(SIGINT, signal_handler);
-	signal(SIGABRT, signal_handler);
-	signal(SIGTERM, signal_handler);
-#else
 	signal(SIGQUIT, signal_handler);
 	signal(SIGTERM, signal_handler);
 	signal(SIGHUP, signal_handler);
 	signal(SIGINT, signal_handler);
-#endif
 
 	/* keep running until the Ctrl+C */
-
 	while (1) {
-	#ifdef WIN32 
-		Sleep(1000);
-	#else
-		sleep (1);
-	#endif
+		sleep (3600);
 	}
 
-	jack_client_close (client);
+	jack_client_close (ctx->client);
 	exit (0);
 }
