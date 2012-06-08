2dcd9078b8003dac996ac3d6f2ab9d9b43fc3cd9
Daniel Thompson <daniel@redfelineninja.org.uk>
2012-06-08 20:51:52 +0100

    libtt/template: Include the standard functions in the template.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/libtt/include/libtt/template.h
+++ src/libtt/include/libtt/template.h
@@ -14,4 +14,21 @@
 #ifndef TT_TEMPLATE_H_
 #define TT_TEMPLATE_H_
 
+#include "libtt/util.h"
+
+typedef struct {
+	tt_context_t *ctx;
+
+} tt_template_t;
+
+void tt_template_init(tt_template_t *p, tt_context_t *ctx);
+void tt_template_finalize(tt_template_t *p);
+tt_template_t *tt_template_new(tt_context_t *ctx);
+void tt_template_delete(tt_template_t *p);
+
+void tt_template_setup(tt_template_t *p);
+
+ttspl_t tt_template_step(tt_template_t *p, ttspl_t spl);
+void tt_template_process(tt_template_t *p, tt_sbuf_t *inbuf, tt_sbuf_t *outbuf);
+
 #endif // TT_TEMPLATE_H_
--- src/libtt/template.c
+++ src/libtt/template.c
@@ -1,5 +1,5 @@
 /*
- * <filename>
+ * template.c
  *
  * Part of libtt (the integer amplifier library)
  *
@@ -10,3 +10,33 @@
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  */
+
+#include <string.h>
+
+#include "libtt.h"
+#include "libtt/template.h"
+
+void tt_template_init(tt_template_t *p, tt_context_t *ctx)
+{
+	memset(p, 0, sizeof(*p));
+}
+
+void tt_template_finalize(tt_template_t *p)
+{
+}
+
+tt_generic_new(template);
+tt_generic_delete(template);
+
+void tt_template_setup(tt_template_t *p)
+{
+}
+
+ttspl_t tt_template_step(tt_template_t *p, ttspl_t spl)
+{
+	return spl;
+}
+
+void tt_template_process(tt_template_t *p, tt_sbuf_t *inbuf, tt_sbuf_t *outbuf)
+{
+}
