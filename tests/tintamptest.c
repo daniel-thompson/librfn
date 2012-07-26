3b314cad71e993f84d8f147faf006dad6b8ad1a7
Daniel Thompson <daniel@redfelineninja.org.uk>
2012-07-26 20:59:07 +0100

    tintamptest: Tidy up the preamble
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- tests/tintamptest.c
+++ tests/tintamptest.c
@@ -1,25 +1,4 @@
 /*
- * templatetest.c
- *
- * Part of libtt (the integer amplifier library)
- *
- * Copyright (C) 2012 Daniel Thompson <daniel@redfelineninja.org.uk> 
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- */
-
-#undef NDEBUG
-
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <librfn.h>
-#include <libtt.h>
-/*
  * tintamptest.c
  *
  * Part of libtt (the integer amplifier library)
@@ -78,6 +57,7 @@ int main()
 
 	// INIT
 	tt_context_t *ctx = tt_context_new();
+	ctx->sampling_frequency = 48000;
 	tt_sbuf_t *inbuf = tt_sbuf_new(ctx->grain_size);
 	tt_sbuf_t *outbuf = tt_sbuf_new(ctx->grain_size);
 	tt_siggen_t *sg = tt_siggen_new(ctx);
