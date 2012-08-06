8f30850a0785c0317ceaf9285cdb04559c33ea3f
Daniel Thompson <daniel@redfelineninja.org.uk>
2012-08-06 21:40:46 +0100

    tests/wavreadertest: Allow the input file to be specified on the command line
    This makes no difference when the test is run as part of the test harness
    but allows arbitrary .wav files to be pushed through the DSP.
    
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- tests/wavreadertest.c
+++ tests/wavreadertest.c
@@ -20,8 +20,16 @@
 #include <librfn.h>
 #include <libtt.h>
 
-int main()
+int main(int argc, char **argv)
 {
+	int cycle_limit = 200;
+	char *fname = "wavreadertest.wav";
+
+	if (argc >= 2) {
+		fname = argv[1];
+		cycle_limit = 0;
+	}
+
 	// INIT
 	tt_context_t *ctx = tt_context_new();
 	tt_wavreader_t *wr = tt_wavreader_new(ctx);
@@ -35,7 +43,7 @@ int main()
 	tt_quantizer_t *q = tt_quantizer_new(ctx);
 	tt_tintamp_t *tt = tt_tintamp_new(ctx);
 
-	int res = tt_wavreader_setup(wr, "wavreadertest.wav");
+	int res = tt_wavreader_setup(wr, fname);
 	assert(0 == res);
 
 	res = tt_wavwriter_setup(ww, "wavwritertest.wav", RF_WAVHEADER_S32LE);
@@ -47,14 +55,14 @@ int main()
 	tt_quantizer_setup(q, 14);
 
 	int i;
-	for (i=0; wr->file && i<200; i++) {
+	for (i=0; wr->file && (cycle_limit==0 || i<cycle_limit); i++) {
 		tt_wavreader_process(wr, outbuf);
 		tt_quantizer_process(q, outbuf, inbuf);
 		tt_tintamp_process(tt, inbuf, outbuf);
 		tt_wavwriter_process(ww, outbuf);
 	}
 	printf("Completed after %d loops\n", i);
-	assert(i == 200);
+	assert(cycle_limit == 0 || i == cycle_limit);
 
 	// TIDY
 	tt_tintamp_delete(tt);
