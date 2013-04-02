6c92cafc4023b2dd1401ed158575c386780e81d1
Daniel Thompson <daniel@redfelineninja.org.uk>
2013-04-02 19:08:23 +0100

    libtt/template: Update the copyright comment for 2013
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/libtt/tests/templatetest.c
+++ src/libtt/tests/templatetest.c
@@ -3,7 +3,7 @@
  *
  * Part of libtt (the integer amplifier library)
  *
- * Copyright (C) 2012 Daniel Thompson <daniel@redfelineninja.org.uk> 
+ * Copyright (C) 2013 Daniel Thompson <daniel@redfelineninja.org.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
