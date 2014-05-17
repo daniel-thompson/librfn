54413eb7b155a34c751643aad4f8d2911cae2af5
Daniel Thompson <daniel@redfelineninja.org.uk>
2014-05-17 10:21:47 +0100

    util: Module and partial function documentation.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- include/librfn/util.h
+++ include/librfn/util.h
@@ -3,7 +3,7 @@
  *
  * Part of librfn (a general utility library from redfelineninja.org.uk)
  *
- * Copyright (C) 2012 Daniel Thompson <daniel@redfelineninja.org.uk> 
+ * Copyright (C) 2012-2014 Daniel Thompson <daniel@redfelineninja.org.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -15,19 +15,26 @@
 #define RF_UTIL_H_
 
 #include <assert.h>
-#include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdint.h>
 
-/**
+/*!
+ * \defgroup librfn_util Miscellaneous utilities
+ *
+ * \brief Utility functions that did not fit into other modules.
+ *
+ * @{
+ */
+
+/*!
  * This macro is similar to the Linux kernel's container_of() macro but
  * doesn't use the GNU extension needed to assure type safety.
  */
 #define containerof(ptr, type, member) \
 	((type *) (((char *) ptr) - offsetof(type, member)))
 
-/**
+/*!
  * Determine the number of elements in a statically allocated array.
  *
  * There's no compile-time checks to this macro. Abuse it and you'll just
@@ -48,14 +55,20 @@
 #endif
 
 /*!
- * @returns >1 if a > b
+ * \brief Compares values that may be subject to overflow.
+ *
+ * This comparision assumes that 0 > 0xffffffff because, in 32-bit unsigned
+ * maths, 0 == 0xffffffff + 1. This is particular useful for working with
+ * 32-bit representations of time.
+ *
+ * @returns >0 if a > b
  * @returns 0 if a == b
- * @returns <1 if a < b
+ * @returns <0 if a < b
  */
 int32_t cyclecmp32(uint32_t a, uint32_t b);
 
 void rf_internal_out_of_memory(void);
-
 void *xmalloc(size_t sz);
 
+/*! @} */
 #endif // RF_UTIL_H_
