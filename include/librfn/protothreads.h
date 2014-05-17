355a5ae23370abb2e223ad1e45b0775c09655984
Daniel Thompson <daniel@redfelineninja.org.uk>
2014-05-17 10:17:26 +0100

    protothreads: Module documentation.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- include/librfn/protothreads.h
+++ include/librfn/protothreads.h
@@ -3,7 +3,7 @@
  *
  * Part of librfn (a general utility library from redfelineninja.org.uk)
  *
- * Copyright (C) 2013 Daniel Thompson <daniel@redfelineninja.org.uk>
+ * Copyright (C) 2013-2014 Daniel Thompson <daniel@redfelineninja.org.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -17,7 +17,44 @@
 #include <assert.h>
 #include <stdint.h>
 
-/*
+/*!
+ * \defgroup librfn_protothreads Protothreads
+ *
+ * \brief Cooperative multi-tasking without context switching
+ *
+ * Protothreads rely on the normal function call mechanism to switch
+ * contexts, however macros provides by the protothread implementation
+ * result in execution continuing from the point the thread yielded
+ * the processor.
+ *
+ * This very simple example does not require a scheduler. Instead the blink
+ * function can be called from a polling loop (or systick interrupt) along
+ * with all system activity.
+ *
+ * \code
+ * int blink_led(void) {
+ *	static pt_state_t ps;
+ *	static uint64_t t;
+ *
+ *	PT_INIT(&ps);
+ *
+ *      t = time64_now();
+ *	while (true) {
+ *		led_toggle();
+ *		t += 250000;
+ *
+ *		PT_WAIT_UNTIL(time64_now() > t);
+ *	}
+ *
+ *	PT_END();
+ * }
+ * \endcode
+ *
+ * \note In dynamic environments where the blink_led() routine is used to
+ *       implement more than one thread then the pt_state_t and the time, t,
+ *       must be passed in as an argument (usually as part of a context
+ *       structure) rather than being static variables.
+ *
  * Like all protothreads implementations this work is inspired by the paper
  * "Protothreads: Simplifying Event-Driven Programming of Memory-Constrained
  * Embedded Systems" by Dunkels et al.
@@ -28,7 +65,7 @@
  * 1. Stronger distinction between yielding and waiting. This allows a
  *    scheduler to handle the cases differently. The fibre scheduler included
  *    in this library observes this distinction. We also eliminate the local
- *    continuation macros concept allowing a implementation of PT_YIELD()
+ *    continuation macros concept allowing an implementation of PT_YIELD()
  *    with no state variable.
  *
  * 2. PT_INIT() saves the protothread pointer to simplify the usage of the other
@@ -95,4 +132,5 @@ typedef uint16_t pt_t;
                 	return ptres; \
 	} while (0)
 
+/*! @} */
 #endif // RF_PROTOTHREADS_H_
