175a40ad280d10189b35fd6516e85077db0357ff
Daniel Thompson <daniel@redfelineninja.org.uk>
2014-05-17 10:15:56 +0100

    hex: Module documentation.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- include/librfn/hex.h
+++ include/librfn/hex.h
@@ -3,7 +3,7 @@
  *
  * Part of librfn (a general utility library from redfelineninja.org.uk)
  *
- * Copyright (C) 2012 Daniel Thompson <daniel@redfelineninja.org.uk> 
+ * Copyright (C) 2012-2014 Daniel Thompson <daniel@redfelineninja.org.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -17,7 +17,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*!
+ * \defgroup librfn_hex Hex dump
+ *
+ * \brief Hexidecimal dump to stdout or a FILE.
+ *
+ * @{
+ */
+
 int rf_hexdump(unsigned char *p, size_t sz);
 int rf_fhexdump(FILE *f, unsigned char *p, size_t sz);
 
+/*! @} */
 #endif // RF_HEX_H_
