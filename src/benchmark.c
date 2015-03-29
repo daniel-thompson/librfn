b1c55020973e00c0a11562952c119c200b4999f1
Daniel Thompson <daniel@redfelineninja.org.uk>
2015-03-29 09:28:49 +0100

    benchmark: Split out into generic code and glue code
    libbench can, in principle, be reused on microcontrollers and other
    difficult environments where the results may be displayed in a
    different way.
    
    Note that this has not yet been tested. The code has merely been teased
    apart to make this possible.
    
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/benchmark.c
+++ src/benchmark.c
@@ -16,76 +16,10 @@
 #include <string.h>
 
 #include "librfn.h"
-
-static fibre_t *next_action;
-
-typedef struct {
-	uint32_t start_time;
-	uint32_t end_time;
-	unsigned int cycles;
-	unsigned int count;
-	fibre_t fibre;
-} benchmark_fibre_t;
-
-int yield_fibre(fibre_t *fibre)
-{
-	benchmark_fibre_t *bm = containerof(fibre, benchmark_fibre_t, fibre);
-
-	PT_BEGIN_FIBRE(fibre);
-
-	bm->start_time = time_now();
-	bm->count = 0;
-
-	while (bm->count++ < bm->cycles)
-		PT_YIELD();
-
-	bm->end_time = time_now();
-	fibre_run(next_action);
-	PT_END();
-}
-
-benchmark_fibre_t single_yield = {
-	.cycles = 1000000,
-	.fibre = FIBRE_VAR_INIT(yield_fibre)
-};
-
-benchmark_fibre_t paired_yield[2] = {
-	{
-		.cycles = 500000,
-		.fibre = FIBRE_VAR_INIT(yield_fibre)
-	},
-	{
-		.cycles = 500000,
-		.fibre = FIBRE_VAR_INIT(yield_fibre)
-	}
-};
-
-typedef struct {
-	stats_t single;
-	stats_t paired;
-} benchmark_results_t;
-
-int benchmark_once(pt_t *pt, benchmark_results_t *results)
-{
-	PT_BEGIN(pt);
-
-	fibre_run(&single_yield.fibre);
-	PT_WAIT();
-	stats_add(&results->single,
-		  single_yield.end_time - single_yield.start_time);
-
-	fibre_run(&paired_yield[0].fibre);
-	fibre_run(&paired_yield[1].fibre);
-	PT_WAIT();
-	stats_add(&results->paired,
-		  paired_yield[1].end_time - paired_yield[0].start_time);
-
-	PT_END();
-}
+#include "libbench.h"
 
 typedef struct {
 	fibre_t fibre;
-	pt_t spawnee;;
 	benchmark_results_t results;
 } conductor_fibre_t;
 
@@ -97,8 +31,7 @@ int conductor_fibre(fibre_t *fibre)
 
 	PT_BEGIN_FIBRE(&c->fibre);
 
-	stats_init(&c->results.single);
-	stats_init(&c->results.paired);
+	benchmark_init(&c->results, fibre);
 
 	/* busy wait for two seconds to try and open up the cpufreq governor */
 	printf("Preparing ...");
@@ -115,16 +48,11 @@ int conductor_fibre(fibre_t *fibre)
 			printf(".");
 			fflush(stdout);
 		}
-		PT_SPAWN(&c->spawnee, benchmark_once(&c->spawnee, &c->results));
+		PT_SPAWN(&c->results.pt, benchmark_run_once(&c->results));
 	}
 	printf(" done\n\n");
 
-	printf("Test                   Min      Mean       Max\n");
-	printf("----------------------------------------------\n");
-	printf("Single          %10d%10d%10d\n", c->results.single.min,
-	       stats_mean(&c->results.single), c->results.single.max);
-	printf("Paired          %10d%10d%10d\n", c->results.paired.min,
-	       stats_mean(&c->results.paired), c->results.paired.max);
+	benchmark_show_results(&c->results);
 
 	exit(0);
 	PT_END();
@@ -136,7 +64,6 @@ conductor_fibre_t conductor = {
 
 int main(int argc, char *argv[])
 {
-	next_action = &conductor.fibre;
 	fibre_run(&conductor.fibre);
 	fibre_scheduler_main_loop();
 
---
+++ src/libbench.c
@@ -0,0 +1,122 @@
+/*
+ * libbench.c
+ *
+ * Part of librfn (a general utility library from redfelineninja.org.uk)
+ *
+ * Copyright (C) 2015 Daniel Thompson <daniel@redfelineninja.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "librfn.h"
+#include "libbench.h"
+
+static fibre_t *next_action;
+
+typedef struct {
+	uint32_t start_time;
+	uint32_t end_time;
+	unsigned int cycles;
+	unsigned int count;
+	fibre_t fibre;
+} benchmark_fibre_t;
+
+static int yield_fibre(fibre_t *fibre)
+{
+	benchmark_fibre_t *bm = containerof(fibre, benchmark_fibre_t, fibre);
+
+	PT_BEGIN_FIBRE(fibre);
+
+	bm->start_time = time_now();
+	bm->count = 0;
+
+	while (bm->count++ < bm->cycles)
+		PT_YIELD();
+
+	bm->end_time = time_now();
+	fibre_run(next_action);
+	PT_END();
+}
+
+static benchmark_fibre_t single_yield = {
+	.cycles = 1000000,
+	.fibre = FIBRE_VAR_INIT(yield_fibre)
+};
+
+static benchmark_fibre_t paired_yield[2] = {
+	{
+		.cycles = 500000,
+		.fibre = FIBRE_VAR_INIT(yield_fibre)
+	},
+	{
+		.cycles = 500000,
+		.fibre = FIBRE_VAR_INIT(yield_fibre)
+	}
+};
+
+void benchmark_init(benchmark_results_t *results, fibre_t *wakeup)
+{
+	memset(results, 0, sizeof(*results));
+	stats_init(&results->single);
+	stats_init(&results->paired);
+	results->wakeup = wakeup;
+}	
+
+int benchmark_run_once(benchmark_results_t *results)
+{
+	/* next_action is a bit of a hack but since it is meaningless to run
+	 * two benchmarks concurrently it is sufficient to unconditionally
+	 * update this every time we run.
+	 */
+	next_action = results->wakeup;
+
+	PT_BEGIN(&results->pt);
+
+	fibre_run(&single_yield.fibre);
+	PT_WAIT();
+	stats_add(&results->single,
+		  single_yield.end_time - single_yield.start_time);
+
+	fibre_run(&paired_yield[0].fibre);
+	fibre_run(&paired_yield[1].fibre);
+	PT_WAIT();
+	stats_add(&results->paired,
+		  paired_yield[1].end_time - paired_yield[0].start_time);
+
+	PT_END();
+}
+
+const char *benchmark_get_result(benchmark_results_t *results, int n,
+				 stats_t *s)
+{
+	switch (n) {
+	case 0:
+		memcpy(s, &results->single, sizeof(*s));
+		return "Single";
+	case 1:
+		memcpy(s, &results->paired, sizeof(*s));
+		return "Paired";
+	default:
+		return NULL;
+	}
+}
+
+void benchmark_show_results(benchmark_results_t *results)
+{
+	const char *name;
+	stats_t result;
+
+	printf("Test                   Min      Mean       Max\n");
+	printf("----------------------------------------------\n");
+
+	for (int i = 0; (name = benchmark_get_result(results, i, &result)); i++)
+		printf("%-16s%10d%10d%10d\n", name, result.min,
+		       stats_mean(&result), result.max);
+}	
