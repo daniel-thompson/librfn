daniel-thompson/librfn

commit cc3c4453afff77143da151b75b6c4a93e6e7bef6

fibre: Provide a fast path for yielding fibres
This patch provides a fast path for a yielding fibre when that fibre
is the only thing on the scheduler (e.g. the runq and timerq are both
empty). This should suit compute intensive fibres seeking to cooperate
with other not-yet-scheduled fibres.

Admission:
  For all the waffling above about compute intensive fibres, this patch
  was actually written to help the libopencm3 USB driver
  for STM32F103 gets polled fast enough to survive enumeration on
  very fast systems (e.g. modern USB3 laptops). The scheduler tweaks
  are only really a hack to avoid implementing USB from an interrupt
  handler.

Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

librfn/fibre.c
@@ -27,6 +27,7 @@ static fibre_t *atomic_runq_buf[8];
 
 static struct {
 	fibre_t *current;
+	fibre_state_t state;
 	uint32_t now;
 
 	list_t runq;
@@ -76,11 +77,11 @@ static fibre_t *get_next_task(void)
 	return containerof(node, fibre_t, link);
 }
 
-static void update_current_state(fibre_state_t state)
+static void update_current_state(void)
 {
-	kernel.current->state = state;
+	kernel.current->state = kernel.state;
 
-	switch (state) {
+	switch (kernel.state) {
 	case FIBRE_STATE_YIELDED:
 		fibre_run(kernel.current);
 		break;
@@ -134,14 +135,27 @@ uint32_t fibre_scheduler_next(uint32_t time)
 {
 	kernel.now = time;
 
-	handle_atomic_runq();
-	handle_timerq();
-
-	kernel.current = get_next_task();
-	if (kernel.current) {
-		fibre_state_t state = kernel.current->fn(kernel.current);
+	/*
+	 * When we have a single fibre yielding to itself we can create a
+	 * fast path by skipping scheduler updates. This allows processor
+	 * intensive work to be harmed as little as possible even when the
+	 * fibre they run in seeks to cooperate with other fibres.
+	 */
+	if (kernel.state != FIBRE_STATE_YIELDED ||
+	    !list_empty(&kernel.runq) ||
+	    !list_empty(&kernel.timerq) ||
+	    !messageq_empty(&kernel.atomic_runq)) {
 		handle_atomic_runq();
-		update_current_state(state);
+		if (kernel.current)
+			update_current_state();
+		handle_timerq();
+		kernel.current = get_next_task();
+	}
+
+	if (kernel.current) {
+		kernel.state = kernel.current->fn(kernel.current);
+		if (kernel.state == FIBRE_STATE_YIELDED)
+			return kernel.now;
 	}
 
 	return get_next_wakeup();

commit b95fec7fdc7bf550adbbb72d24967dcd7bf29df0

fibre: Fix handling of atomic runq during wakeup calculation
Currently it is possible for the scheduler to report an incorrect wakeup
time when a task is held on the atomic runq. This results in the
scheduler going idle prematurely.

This is a regression caused by cc3c4453afff("fibre: Provide a fast path
for yielding fibres).

Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

librfn/fibre.c
@@ -101,7 +101,7 @@ static void update_current_state(void)
 
 static uint32_t get_next_wakeup(void)
 {
-	if (!list_empty(&kernel.runq))
+	if (!messageq_empty(&kernel.atomic_runq) || !list_empty(&kernel.runq))
 		return kernel.now;
 
 	if (list_empty(&kernel.timerq))